fe3c4b025550d215bf0279867295fa61446bcc4e
matthias mitterreiter <matthias@mitterreiter.de>
2019-01-01 20:20:28 +0100

    added tests

--- tests/test_gemv.cc
+++ tests/test_gemv.cc
@@ -3,26 +3,27 @@
 
 #include <gemv/gemv.h>
 #include <iostream>
-using namespace std;
+using namespace matthiasware;
 
-TEST_CASE("lots of nested subcases") {
-    cout << endl << "root" << endl;
-    SUBCASE("") {
-        cout << "1" << endl;
-        SUBCASE("") { cout << "1.1" << endl; }
-    }
-    SUBCASE("") {   
-        cout << "2" << endl;
-        SUBCASE("") { cout << "2.1" << endl; }
-        SUBCASE("") {
-            cout << "2.2" << endl;
-            SUBCASE("") {
-                cout << "2.2.1" << endl;
-                SUBCASE("") { cout << "2.2.1.1" << endl; }
-                SUBCASE("") { cout << "2.2.1.2" << endl; }
-            }
-        }
-        SUBCASE("") { cout << "2.3" << endl; }
-        SUBCASE("") { cout << "2.4" << endl; }
-    }
+TEST_CASE("FIRST")
+{
+	int m = 3;
+	int n = 2;
+	int A[m * n] = {1,2,3,4,5,6};
+	int y[3] = {4, 5, 6};
+	int x[2] = {1, 2};
+	int a {2};
+	int b {3};
+	SUBCASE(""){
+		gemv<int>(A, m, n, a, x, b, y);
+		CHECK(y[0] == 22);
+		CHECK(y[1] == 37);
+		CHECK(y[2] == 52);
+	}
+	SUBCASE(""){
+		gemv<int>(A, m, n, a, x, 0, y);
+		CHECK(y[0] == 10);
+		CHECK(y[1] == 22);
+		CHECK(y[2] == 34);
+	}
 }
